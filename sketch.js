a27e8b04d1d2917fd4750a85abc8ce8486060d0e
alschlee <1daysixmeal@gmail.com>
2025-11-17 09:23:52 +0900

    [FEAT] 시간 입력

--- sketch.js
+++ sketch.js
@@ -1,8 +1,32 @@
+let button;
+let hours;
+let minutes;
+let seconds;
+
 function setup() {
   createCanvas(400, 400);
+
+  hours = createInput();
+  hours.size(20);
+  hours.position(20, 65);
+
+  minutes = createInput();
+  minutes.size(20);
+  minutes.position(50, 65);
+
+  seconds = createInput();
+  seconds.size(20);
+  seconds.position(130, 65);
+
+  button = createButton('submit');
+  button.position(130, 65);
+  button.mousePressed(setTime);
 }
 
 function draw() {
   background(220);
-  text("반갑다", 10, 10);
+}
+
+function setTime() {
+  console.log(hours.value(), minutes.value(), seconds.value());
 }
